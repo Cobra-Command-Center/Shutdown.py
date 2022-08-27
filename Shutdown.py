aecc1ad88af686bc216176596a8cfb33b83d993c
William Shaw <112270867+Cobra-Command-Center@users.noreply.github.com>
2022-08-27 16:27:17 -0400

    Initial upload
    Example file free to use.

---
+++ Shutdown.py
@@ -0,0 +1,11 @@
+# Shutdown.py helps shutdown your Windows computer using PowerShell and Python
+import socket
+import os
+
+# Obtains Computer Hostname, Designate Shell Environment, Establishes Command
+hostname = socket.gethostname()
+shell = "powershell.exe"
+command = "Stop-Computer -ComputerName " + hostname + "-Force"
+
+# Executes PowerShell command Stop-Computer -ComputerName [ hostname ] -Force
+os.system(shell + " " + command)
